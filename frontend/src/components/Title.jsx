4f648d1dd1b28d19b4dc3f2ed9e2cf75751c265a
Ali Issa <alyy.issa01@gmail.com>
2025-10-25 14:39:30 +0300

    Added Title.jsx file

---
+++ frontend/src/components/Title.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Title = () => {
+  return (
+    <div class="mb-16 text-center">
+      <span class="py-1 px-4 bg-indigo-100 rounded-full text-xs font-medium text-indigo-600">
+        Features
+      </span>
+      <h2 class="text-4xl text-center font-bold text-gray-900 py-5">
+        Why Choose Our CS Courses?
+      </h2>
+      <p class="text-lg font-normal text-gray-500 max-w-md md:max-w-2xl mx-auto">
+        Explore interactive lessons, hands-on exercises, and real university-level exams built to help you master every CS subject.
+      </p>
+    </div>
+  )
+}
+
+export default Title
