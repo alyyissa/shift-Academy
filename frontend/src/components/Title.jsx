ca223afc8f8da65e343a966e8eb49ad66f91878a
Ali Issa <alyy.issa01@gmail.com>
2025-10-25 15:11:18 +0300

    Added styles for section1

--- frontend/src/components/Title.jsx
+++ frontend/src/components/Title.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
-const Title = () => {
+const Title = ({smallTitle, Title, Desc}) => {
   return (
     <div class="mb-16 text-center">
       <span class="py-1 px-4 bg-indigo-100 rounded-full text-xs font-medium text-indigo-600">
-        Features
+        {smallTitle}
       </span>
       <h2 class="text-4xl text-center font-bold text-gray-900 py-5">
-        Why Choose Our CS Courses?
+        {Title}
       </h2>
       <p class="text-lg font-normal text-gray-500 max-w-md md:max-w-2xl mx-auto">
-        Explore interactive lessons, hands-on exercises, and real university-level exams built to help you master every CS subject.
+        {Desc}
       </p>
     </div>
   )
