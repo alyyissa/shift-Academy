d948805eb63480654eda73969703a3414ad8cdd5
Ali Issa <alyy.issa01@gmail.com>
2025-10-26 15:38:09 +0200

    Added breadcrumps to track the path

--- frontend/src/pages/Courses.jsx
+++ frontend/src/pages/Courses.jsx
@@ -8,20 +8,34 @@ const Courses = () => {
         Title="Prepare, Practice, and Excel in Your Studies"
         Desc="Access tailored exercises, quizzes, and tests to help you strengthen your understanding before or during the semester. Practice smarter and stay ready for every exam"
         />
-        <div className='flex flex-col md:flex-row gap-5 md:gap-20 my-10'>
+        <div className='flex flex-col md:flex-row gap-5 md:gap-20 my-10  items-center w-full justify-center'>
             <div className="flex justify-center">
                 <div className="flex items-center bg-white p-1 border border-gray-500/50 text-sm w-auto rounded-full">
                     <div className="flex items-center">
                         <input
                             type="radio"
                             name="options"
-                            id="html"
+                            id="all"
                             className="hidden peer"
                             defaultChecked
                         />
                         <label
+                            htmlFor="all"
+                            className="cursor-pointer rounded-full py-2 px-4 md:px-7 text-gray-500 transition-colors duration-200 peer-checked:bg-coprimary peer-checked:text-white"
+                        >
+                            All Courses
+                        </label>
+                    </div>
+                    <div className="flex items-center">
+                        <input
+                            type="radio"
+                            name="options"
+                            id="html"
+                            className="hidden peer"
+                        />
+                        <label
                             htmlFor="html"
-                            className="cursor-pointer rounded-full py-2 px-8 text-gray-500 transition-colors duration-200 peer-checked:bg-coprimary peer-checked:text-white"
+                            className="cursor-pointer rounded-full py-2 px-4 md:px-7 text-gray-500 transition-colors duration-200 peer-checked:bg-coprimary peer-checked:text-white"
                         >
                             Major
                         </label>
@@ -35,7 +49,7 @@ const Courses = () => {
                         />
                         <label
                             htmlFor="css"
-                            className="cursor-pointer rounded-full py-2 px-8 text-gray-500 transition-colors duration-200 peer-checked:bg-coprimary peer-checked:text-white"
+                            className="cursor-pointer rounded-full py-2 px-4 md:px-7 text-gray-500 transition-colors duration-200 peer-checked:bg-coprimary peer-checked:text-white"
                         >
                             Maths
                         </label>
@@ -49,7 +63,7 @@ const Courses = () => {
                         />
                         <label
                             htmlFor="react"
-                            className="cursor-pointer rounded-full py-2 px-8 text-gray-500 transition-colors duration-200 peer-checked:bg-coprimary peer-checked:text-white"
+                            className="cursor-pointer rounded-full py-2 px-4 md:px-7 text-gray-500 transition-colors duration-200 peer-checked:bg-coprimary peer-checked:text-white"
                         >
                             Elective
                         </label>
@@ -60,7 +74,7 @@ const Courses = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="#6B7280">
                         <path d="M13 3C7.489 3 3 7.489 3 13s4.489 10 10 10a9.95 9.95 0 0 0 6.322-2.264l5.971 5.971a1 1 0 1 0 1.414-1.414l-5.97-5.97A9.95 9.95 0 0 0 23 13c0-5.511-4.489-10-10-10m0 2c4.43 0 8 3.57 8 8s-3.57 8-8 8-8-3.57-8-8 3.57-8 8-8"/>
                     </svg>
-                    <input type="text" className="w-full h-full outline-none text-sm text-gray-500" />
+                    <input type="text" className="w-full h-full outline-none text-sm text-gray-500 placeholder:text-slate-400" placeholder='OOP, Data Structure...'/>
                     <button type="submit" className="bg-coprimary w-32 h-9 rounded-full text-sm text-white mr-[5px]">Search</button>
             </div>
         </div>
