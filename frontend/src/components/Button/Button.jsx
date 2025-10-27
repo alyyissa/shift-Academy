19ce0caf79926929603606ecf3c4302d9a151c66
Ali Issa <alyy.issa01@gmail.com>
2025-10-27 18:16:55 +0200

    Fixed tailwind classes in button.jsx

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -5,7 +5,7 @@ const Button = ({text}) => {
     <div className={`${styles["login-btns"]} text-center mb-5`}>
         <button
         type="submit"
-        className="bg-[#5076db] text-[14px] transition-all duration-[0.3s] ease-in-out font-semibold rounded-[5px] border-[2px] border-solid border-transparent text-[#fff] py-[8px] px-auto my-[8px] cursor-pointer w-[90px] hover:bg-transparent hover:text-[#5076db] hover:border-[#5076db]"
+        className="bg-coprimary text-[14px] transition-all duration-300 ease-in-out font-semibold rounded-[5px] border-2 border-solid border-transparent text-[#fff] py-2 px-auto my-2 cursor-pointer w-[90px] hover:bg-transparent hover:text-coprimary hover:border-coprimary"
         >
             {text}
         </button>
