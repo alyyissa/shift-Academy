814685ffa146eeffece61a61cbf56f76e626239d
Ali Issa <alyy.issa01@gmail.com>
2025-10-31 19:03:06 +0200

    Added DTO for user model

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString, Min, MinLength } from "class-validator"
+
+export class CreateUserDto {
+    id: number
+
+    @IsString({message: 'Name should be a string value'})
+    @IsNotEmpty()
+    @MinLength(3,{message: 'Name should have a minimum 3 characters.'})
+    name: string
+
+    @IsString()
+    gender?: string // that shows that the option is an optional
+
+    @Min(16, {message: "You should be 16 or older"})
+    age:number
+
+    @IsEmail()
+    email: string
+}
--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ValidationPipe } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -8,8 +8,9 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  create(@Body() user: CreateUserDto) {
+    // return this.userService.create(createUserDto);
+    return "A new user is created";
   }
 
   @Get()
