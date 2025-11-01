e2df52a50e89bc48ad95517e748fd3aa6b63d210
Ali Issa <alyy.issa01@gmail.com>
2025-11-01 16:11:42 +0200

    Added question DTOs

--- backend/src/question/dto/create-question.dto.ts
+++ backend/src/question/dto/create-question.dto.ts
@@ -1,6 +1,12 @@
-import { IsNumber } from "class-validator";
+import { IsNumber, IsString } from "class-validator";
 
 export class CreateQuestionDto {
     @IsNumber()
     id: number
+
+    @IsString()
+    question: string
+
+    @IsString()
+    difficulty: string
 }
