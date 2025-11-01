0d0b73e0361a8e5b5b739f4ad9a89ce37896789a
Ali Issa <alyy.issa01@gmail.com>
2025-11-01 16:13:48 +0200

    Added Answer DTOs

--- backend/src/answer/dto/create-answer.dto.ts
+++ backend/src/answer/dto/create-answer.dto.ts
@@ -1,1 +1,14 @@
-export class CreateAnswerDto {}
+import { IsBoolean, IsNumber, IsString } from "class-validator";
+
+export class CreateAnswerDto {
+    @IsNumber()
+    id:number
+
+    @IsString()
+    answer: string
+
+    @IsBoolean()
+    correct_answer: boolean
+
+    // question FK
+}
