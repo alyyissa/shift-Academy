1093f8f582317433070174cf8f231f44ec5dcb33
Ali Issa <alyy.issa01@gmail.com>
2025-11-03 18:26:11 +0200

    Added useFactory for the synchronize of the DB connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -18,15 +18,17 @@ import { RateModule } from './rate/rate.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT as any,
-      username: process.env.DB_USERNAME,
-      password: process.env.PASSWORD, 
-      database: process.env.DATABASE,
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+        useFactory: () => ({
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT as any,
+        username: process.env.DB_USERNAME,
+        password: process.env.PASSWORD, 
+        database: process.env.DATABASE,
+        autoLoadEntities: true,
+        synchronize: true,
+      })
     }),
     UserModule,
     UniversityModule,
