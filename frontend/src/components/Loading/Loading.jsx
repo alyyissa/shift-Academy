cf9096a118d1c7b0f49e1b0981739848cc17dd91
Ali Issa <alyy.issa01@gmail.com>
2026-01-26 16:30:56 +0200

    Added the color to the loading component

--- frontend/src/components/Loading/Loading.jsx
+++ frontend/src/components/Loading/Loading.jsx
@@ -2,7 +2,7 @@ import styles from "./Loading.module.css"
 
 const Loading = () => {
   return (
-    <div className="h-screen w-screen flex bg-[#fff] items-center justify-center fixed top-0">
+    <div className="h-screen w-screen flex bg-white items-center justify-center fixed top-0">
         <div className={`${styles["loader"]} loader h-[50px] w-[50px] rounded-[50%]`}></div>
     </div>
   )
